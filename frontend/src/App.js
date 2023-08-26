b3b065a3eefbaa3e8497ed6beee59a9b602dde9b
adamya-22 <104303051+adamya-22@users.noreply.github.com>
2023-08-26 10:15:14 +0530

    itr page added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,7 @@ import { Routes, Route } from 'react-router-dom'
 import Navbar from './components/Navbar'
 import Homepage from './components/Homepage';
 import Footer from './components/Footer';
+import ItrFiling from './components/ItrFiling';
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
     <Navbar/>
     <Routes>
       <Route path='/' element={<Homepage/>} />
+      <Route path='/' element={<ItrFiling/>} />
     </Routes>
     <Footer/>
     </>
---
+++ frontend/src/components/ItrFiling.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const ItrFiling = () => {
+  return (
+    <div>ItrFiling</div>
+  )
+}
+
+export default ItrFiling
