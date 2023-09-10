1ef114c0c8f1f21cca0c08c820a9c76ece1819cf
adamya-22 <104303051+adamya-22@users.noreply.github.com>
2023-09-10 19:17:14 +0530

    animation section added

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -12,9 +12,9 @@ const Footer = () => {
                 <img width="800" height="206" src="wp-content/uploads/sites/111/2021/06/pajax-logo-6-1024x264.png" class="attachment-large size-large wp-image-3989" alt="" decoding="async" loading="lazy" srcset="https://templatekit.jegtheme.com/pajax/wp-content/uploads/sites/111/2021/06/pajax-logo-6-1024x264.png 1024w, https://templatekit.jegtheme.com/pajax/wp-content/uploads/sites/111/2021/06/pajax-logo-6-300x77.png 300w, https://templatekit.jegtheme.com/pajax/wp-content/uploads/sites/111/2021/06/pajax-logo-6-768x198.png 768w, https://templatekit.jegtheme.com/pajax/wp-content/uploads/sites/111/2021/06/pajax-logo-6-1536x395.png 1536w, https://templatekit.jegtheme.com/pajax/wp-content/uploads/sites/111/2021/06/pajax-logo-6-2048x527.png 2048w, https://templatekit.jegtheme.com/pajax/wp-content/uploads/sites/111/2021/06/pajax-logo-6-800x206.png 800w, https://templatekit.jegtheme.com/pajax/wp-content/uploads/sites/111/2021/06/pajax-logo-6-e1626234217274.png 1920w" sizes="(max-width: 800px) 100vw, 800px" /></a> */}
                 <div className='alt-logo'><h1>FINPORTAL</h1></div>
                 </div>
-            <div class="f12"><a href="#"><b>Address: </b>Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit, et.</a></div>
-            <div class="f12"><a href="#"><b>Phone: </b>+91 8787878787</a></div>
-            <div class="f12"><a href="#"><b>Email: </b>Loremipsum@gmail.com</a></div>
+            <div class="f12"><a href="#"><b>Address: </b>217-218 Rajhans Olympia, Nr Rupali Naher, Bhatar, Surat, Gujarat, India</a></div>
+            <div class="f12"><a href="#"><b>Phone: </b>+91-9824685556</a></div>
+            <div class="f12"><a href="#"><b>Email: </b>info@finportalin.com</a></div>
             <div class="f13">
                 <a href="#" class="f13-font"><i class="fa-brands fa-facebook-f"></i></a>
                 <a href="#" class="f13-font"><i class="fa-brands fa-instagram"></i></a>
@@ -25,7 +25,6 @@ const Footer = () => {
         <div class="f1 f1-cen">
             <div class="f21"><h3>Information</h3></div>
             <div class="f22"><a href="#">About Us</a></div>
-            {/* <div class="f22"><a href="#">Checkout</a></div> */}
             <div class="f22"><a href="#">Contact</a></div>
             <div class="f22"><a href="#">Serivius</a></div>
         </div>
@@ -33,7 +32,6 @@ const Footer = () => {
             <div class="f21"><h3>My Account</h3></div>
             <div class="f22"><a href="#">My Account</a></div>
             <div class="f22"><a href="#">Contact</a></div>
-            {/* <div class="f22"><a href="#">Shopping Cart</a></div> */}
             <div class="f22"><a href="#">Shop</a></div>
         </div>
         <div class="f1">
